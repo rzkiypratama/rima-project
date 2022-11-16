cbf633585b4aaa4554b19e2cba3ee314fb510605
imbasri.dev <imbasri.dev@gmail.com>
2022-11-16 16:28:11 +0700

    add homepage

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+
+// Pages
+import Homepage from "./pages/HomePage";
+
 import App from "./App";
 
-const router = createBrowserRouter ([
-  { path: "/app", element: <App/>}
+const router = createBrowserRouter([
+   //  { path: "/app", element: <App /> },
+   { path: "/", element: <Homepage /> },
 ]);
 
-export default router
+export default router;
