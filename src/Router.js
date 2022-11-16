6b0289392aac20a241f2cfb9f6d47bb11df79105
Rizky Putra Pratama <pratamabusiness@gmail.com>
2022-11-16 23:30:32 +0800

    add new pages

--- src/Router.js
+++ src/Router.js
@@ -3,10 +3,14 @@ import { createBrowserRouter } from "react-router-dom";
 // Pages
 import Homepage from "./pages/HomePage";
 import Account from "./pages/AccountPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import Notification from "./pages/Notification";
 
 const router = createBrowserRouter([
    { path: "/", element: <Homepage /> },
    { path: "/account", element: <Account /> },
+   { path: "/forgot", element: <ForgotPassword /> },
+   { path: "/notification", element: <Notification /> },
 ]);
 
 export default router;
