3dd7c83468951f149dd6e57d8989f37c91794b0a
Rizky Putra Pratama <pratamabusiness@gmail.com>
2022-11-16 17:33:45 +0800

    same routes

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const router = createBrowserRouter ([
-  { path: "/app", element: <App/>}
+  { path: "/", element: <App/>}
 ]);
 
 export default router
