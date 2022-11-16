cd389f9dee377048f27b97419ea87bd317e2539b
Rizky Putra Pratama <pratamabusiness@gmail.com>
2022-11-16 16:33:38 +0800

    dependencies

---
+++ src/Router.js
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter ([
+  { path: "/app", element: <App/>}
+]);
+
+export default router
