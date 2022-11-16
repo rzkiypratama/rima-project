f3679f7a3a7ecfe4eb87a61a553c3d00fe5b0418
MoIkrom <mikram216@gmail.com>
2022-11-16 22:01:45 +0700

    Navbar & Footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 // styles
 function HomePage() {
-   return (
-      <>
-         <h1>Minimal Furniture Storelo</h1>
-      </>
-   );
+  return (
+    <>
+      <h1>Minimal Furniture Storelo</h1>
+    </>
+  );
 }
 
 export default HomePage;
