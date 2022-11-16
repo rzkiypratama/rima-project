9334d4a364f0187cac56f3a37a953092d1494f77
imbasri.dev <imbasri.dev@gmail.com>
2022-11-16 16:50:47 +0700

    fix router

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 
 // Pages
 import Homepage from "./pages/HomePage";
-
-import App from "./App";
+import Account from "./pages/AccountPage";
 
 const router = createBrowserRouter([
-   //  { path: "/app", element: <App /> },
    { path: "/", element: <Homepage /> },
+   { path: "/account", element: <Account /> },
 ]);
 
 export default router;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+// styles
 function HomePage() {
    return (
       <div>
