9b7f8052faa0e84b6ec3eba24ceebea3a120231d
Rizky Putra Pratama <pratamabusiness@gmail.com>
2022-11-17 15:43:06 +0800

    add new pages & routes

--- src/Router.js
+++ src/Router.js
@@ -8,7 +8,6 @@ import Notification from "./pages/Notification";
 import OrderTracking from "./pages/OrderTracking";
 import OrderTrackingDetail from "./pages/OrderTrackingDetail";
 import Chat from "./pages/Chat";
-<<<<<<< HEAD
 import FAQ from "./pages/FAQ";
 import Profile from "./pages/Profile";
 import Product from "./pages/Product";
@@ -22,17 +21,6 @@ const router = createBrowserRouter([
   { path: "/faq", element: <FAQ /> },
   { path: "/profile", element: <Profile /> },
   { path: "/product", element: <Product /> },
-=======
-
-const router = createBrowserRouter([
-   { path: "/", element: <Homepage /> },
-   { path: "/account", element: <Account /> },
-   { path: "/forgot", element: <ForgotPassword /> },
-   { path: "/notification", element: <Notification /> },
-   { path: "/order/tracking", element: <OrderTracking /> },
-   { path: "/order/tracking/detail", element: <OrderTrackingDetail /> },
-   { path: "/chat", element: <Chat /> },
->>>>>>> origin/order-tracking
 ]);
 
 export default router;
