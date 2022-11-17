eb0cfdc205eaa48a66c0ba4c66c41cb251a2aeaa
imbasri.dev <imbasri.dev@gmail.com>
2022-11-17 14:53:05 +0700

    update router

--- src/Router.js
+++ src/Router.js
@@ -8,7 +8,6 @@ import Notification from "./pages/Notification";
 import OrderTracking from "./pages/OrderTracking";
 import OrderTrackingDetail from "./pages/OrderTrackingDetail";
 import Chat from "./pages/Chat";
-<<<<<<< HEAD
 
 const router = createBrowserRouter([
    { path: "/", element: <Homepage /> },
@@ -18,19 +17,6 @@ const router = createBrowserRouter([
    { path: "/order/tracking", element: <OrderTracking /> },
    { path: "/order/tracking/detail", element: <OrderTrackingDetail /> },
    { path: "/chat", element: <Chat /> },
-=======
-import FAQ from "./pages/FAQ";
-import Profile from "./pages/Profile";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Homepage /> },
-  { path: "/account", element: <Account /> },
-  { path: "/forgot", element: <ForgotPassword /> },
-  { path: "/notification", element: <Notification /> },
-  { path: "/chat", element: <Chat /> },
-  { path: "/faq", element: <FAQ /> },
-  { path: "/profile", element: <Profile /> },
->>>>>>> 836d33c35e10201c4a9d2e26961649aa3edcedd5
 ]);
 
 export default router;
