3ccb28354a24112387a229788e787f37a68bee4c
MoIkrom <mikram216@gmail.com>
2022-11-18 16:52:59 +0700

    fix navbar & footer

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -4,17 +4,36 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import styles from "../navbar/Navbar.module.css";
 import { Link } from "react-router-dom";
+import NavbarRegister from "./component/NavbarRegister";
+// import { useState } from "react";
+
+import wishlist from "../navbar/wishlist.png";
+import search from "../navbar/search.png";
+import cart from "../navbar/cart.png";
 
 function BasicExample() {
+  // const [navbar, setnavbar] = useState(NavbarLogin);
+  // navType = () => {
+  //   if (this.state.userInfo.token) {
+  //     if (this.state.userInfo.role === "user") {
+  //       return this.state.navLogin;
+  //     } else {
+  //       return this.state.navAdmin;
+  //     }
+  //   } else {
+  //     return this.state.navnotLogin;
+  //   }
+  // };
   return (
     <Navbar expand="lg">
       <Container>
         <Navbar.Brand className={`${styles["company"]}`} href="#home">
           RIMA
+          <div className="dropdown"></div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto gap-5">
+          <Nav className={`mx-auto gap-5 ${styles["navbar"]}`}>
             <Nav.Link className={styles["title"]} href="/">
               HOME
             </Nav.Link>
@@ -22,10 +41,13 @@ function BasicExample() {
               <Link className={styles["no-underline"]} to={"/about"}>
                 <NavDropdown.Item href="#action/3.1">About Us </NavDropdown.Item>
               </Link>
+
               <NavDropdown.Item href="#action/3.2">Contact Us</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Coming Soon </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">404 Page </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">FAQ Page</NavDropdown.Item>
+              <Link className={styles["no-underline"]} to={"/faq"}>
+                <NavDropdown.Item href="#action/3.1">FAQ Page</NavDropdown.Item>
+              </Link>
             </NavDropdown>
             <NavDropdown className={styles["title"]} title="SHOP" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -34,23 +56,16 @@ function BasicExample() {
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
+
             <Nav.Link className={styles["title"]} href="#home">
               BLOG
             </Nav.Link>
           </Nav>
           <div className={` d-flex gap-5 ${styles["icon"]}`}>
-            <svg className={`${styles["svg"]}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
-            </svg>
-            <svg className={`${styles["svg"]}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
-            </svg>
-            <svg className={`${styles["svg"]}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-              <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-            </svg>
-            <svg className={`${styles["svg"]} ${styles["toggle"]}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-            </svg>
+            <img className={`  ${styles["icon-1"]} ${styles["cursor"]}`} src={search} alt="/" />
+            <img className={`   ${styles["cursor"]}`} src={wishlist} alt="/" />
+            <img className={`   ${styles["cursor"]}`} src={cart} alt="/" />
+            <NavbarRegister />
           </div>
         </Navbar.Collapse>
       </Container>
