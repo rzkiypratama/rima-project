10de5606b7e842a04489df73c29421f207d7cd24
MoIkrom <mikram216@gmail.com>
2022-11-21 03:38:12 +0700

    edit

--- src/pages/SellerProduct.jsx
+++ src/pages/SellerProduct.jsx
@@ -12,168 +12,123 @@ import { Link } from "react-router-dom";
 // import NavigationSeller from "../component/navigationSeller/NavigationSeller";
 
 function SellerProduct() {
-   const url = `${process.env.REACT_APP_BACKEND_HOST}/product`;
+  const url = `${process.env.REACT_APP_BACKEND_HOST}/product`;
 
-   const [stock, setStock] = useState("");
-   const [desc, setDesc] = useState("");
-   const [price, setPrice] = useState("");
-   const [image, setImage] = useState("");
-   const [data, setData] = useState("");
-   useEffect(() => {
-      const url = `${process.env.REACT_APP_BACKEND_HOST}/product`;
-      axios
-         .get(url)
-         .then((res) => {
-            setStock(res.data.data.data.stock);
-            setDesc(res.data.data.data.description);
-            setPrice(res.data.data.data.price);
-            setImage(res.data.data.data.image);
-            setData(res.data.data.data);
-            console.log("data : ", res.data.data.data);
-         })
-         .catch((err) => {
-            console.log(err);
-         });
-   }, []);
-   const HandleProduct = () => {
-      return axios
-         .get(url)
-         .then((res) => {
-            setStock(res.data.data.data.stock);
-            setDesc(res.data.data.data.description);
-            setPrice(res.data.data.data.price);
-            setImage(res.data.data.data.image);
-            setData(res.data.data.data);
-            console.log("data : ", res.data.data.data);
-         })
-         .catch((err) => {
-            console.log(err);
-         });
-   };
+  const [stock, setStock] = useState("");
+  const [desc, setDesc] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_BACKEND_HOST}/product`;
+    axios
+      .get(url)
+      .then((res) => {
+        setStock(res.data.data.data.stock);
+        setDesc(res.data.data.data.description);
+        setPrice(res.data.data.data.price);
+        setImage(res.data.data.data.image);
+        setData(res.data.data.data);
+        //   console.log("data : ", res.data.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const HandleProduct = () => {
+    return axios
+      .get(url)
+      .then((res) => {
+        setStock(res.data.data.data.stock);
+        setDesc(res.data.data.data.description);
+        setPrice(res.data.data.data.price);
+        setImage(res.data.data.data.image);
+        setData(res.data.data.data);
+        console.log("data : ", res.data.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-   const costing = (price) => {
-      return parseFloat(price)
-         .toFixed()
-         .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-   };
+  const costing = (price) => {
+    return parseFloat(price)
+      .toFixed()
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+  };
 
-   return (
-      <div>
-         <Navbar />
-         <div className={`container-fluid p-5 ${styles["cont-fluid"]}`}>
-            <div className="container justify-content-center">
-               <p className={`text-center  ${styles["profile"]}`}>My Product</p>
-               <p className={`text-center fs-6 ${styles["text-profile"]}`}>
-                  See your notifications for the latest updates
-               </p>
-            </div>
-         </div>
-         <div className=" container justify-content-evenly d-flex  my-5">
-            <p className="nav-item">
-               <Link
-                  className={` text-decoration-none ${styles["no-underline"]}`}
-                  to={"/profile"}
-               >
-                  <p className={`nav-link ${styles["color-text"]}`}>Profile</p>
-               </Link>
-            </p>
-
-            <div
-               className={`d-flex gap-2 align-items-center ${styles["cursor"]} ${styles["color"]}`}
-            >
-               <p
-                  className={`${styles["cursor"]} ${styles["color-text"]}`}
-                  onClick={HandleProduct}
-               >
-                  My Product
-               </p>
-               <NavDropdown
-                  className={` ${styles["menu"]} ${styles["color-text"]}`}
-                  id="basic-nav-dropdown"
-               >
-                  <NavDropdown.Item onClick={HandleProduct}>
-                     All
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>Archive</NavDropdown.Item>
-                  <NavDropdown.Item>Sold Out</NavDropdown.Item>
-               </NavDropdown>
-            </div>
-            <p className="nav-item">
-               <Link
-                  className={` text-decoration-none ${styles["no-underline"]} `}
-                  to={"/admin/create-product"}
-               >
-                  <p className={`nav-link ${styles["color-text"]}`}>
-                     Selling Product
-                  </p>
-               </Link>
-            </p>
-            <div
-               className={`d-flex gap-2 align-items-center ${styles["cursor"]} ${styles["color"]}`}
-            >
-               <Link
-                  to={"/admin/my-order"}
-                  className={` text-decoration-none ${styles["no-underline"]} `}
-               >
-                  <p className={`${styles["cursor"]} ${styles["color-text"]}`}>
-                     My Order
-                  </p>
-               </Link>
-               <NavDropdown
-                  className={` ${styles["menu"]} ${styles["color-text"]}`}
-                  id="basic-nav-dropdown"
-               >
-                  <NavDropdown.Item onClick={HandleProduct}>
-                     All
-                  </NavDropdown.Item>
-                  <NavDropdown.Item>Archive</NavDropdown.Item>
-                  <NavDropdown.Item>Sold Out</NavDropdown.Item>
-               </NavDropdown>
-            </div>
-         </div>
-
-         <hr className="container" />
-         <section className="container d-flex px-5 justify-content-between ">
-            <p
-               className={`${styles["title"]} col-4 d-flex justify-content-start ms-4 `}
-            >
-               Product
-            </p>
-            <p
-               className={`${styles["title"]} col-4 d-flex justify-content-start ms-4 `}
-            >
-               {" "}
-               Stock Status
-            </p>
-            <p
-               className={`${styles["title"]} col-4 d-flex justify-content-start ms-4 `}
-            >
-               Price
-            </p>
-         </section>
-         <hr className="container" />
-         <div className="container">
-            {data.length > 0 ? (
-               data.map((data, index) => {
-                  return (
-                     <Table
-                        key={index}
-                        stock={data.stock}
-                        description={data.desc}
-                        image={data.image}
-                        price={`${"IDR"} ${costing(data.price)}`}
-                     />
-                  );
-               })
-            ) : (
-               <>
-                  <Loader />
-               </>
-            )}
-         </div>
-         <Footer />
+  return (
+    <div>
+      <Navbar />
+      <div className={`container-fluid p-5 ${styles["cont-fluid"]}`}>
+        <div className="container justify-content-center">
+          <p className={`text-center  ${styles["profile"]}`}>My Product</p>
+          <p className={`text-center fs-6 ${styles["text-profile"]}`}>See your notifications for the latest updates</p>
+        </div>
       </div>
-   );
+      <div className=" container justify-content-evenly d-flex  my-5">
+        <p className="nav-item">
+          <Link className={` text-decoration-none ${styles["no-underline"]}`} to={"/profile"}>
+            <p className={`nav-link ${styles["color-text"]}`}>Profile</p>
+          </Link>
+        </p>
+
+        <div className={`d-flex gap-2 align-items-center ${styles["cursor"]} ${styles["color"]}`}>
+          <p className={`${styles["cursor"]} ${styles["color-text"]}`} onClick={HandleProduct}>
+            My Product
+          </p>
+          <NavDropdown className={` ${styles["menu"]} ${styles["color-text"]}`} id="basic-nav-dropdown">
+            <NavDropdown.Item onClick={HandleProduct}>All</NavDropdown.Item>
+            <NavDropdown.Item>Archive</NavDropdown.Item>
+            <NavDropdown.Item>Sold Out</NavDropdown.Item>
+          </NavDropdown>
+        </div>
+        <p className="nav-item">
+          <Link className={` text-decoration-none ${styles["no-underline"]} `} to={"/admin/create-product"}>
+            <p className={`nav-link ${styles["color-text"]}`}>Selling Product</p>
+          </Link>
+        </p>
+        <div className={`d-flex gap-2 align-items-center ${styles["cursor"]} ${styles["color"]}`}>
+          <Link to={"/admin/my-order"} className={` text-decoration-none ${styles["no-underline"]} `}>
+            <p className={`${styles["cursor"]} ${styles["color-text"]}`}>My Order</p>
+          </Link>
+          <NavDropdown className={` ${styles["menu"]} ${styles["color-text"]}`} id="basic-nav-dropdown">
+            <NavDropdown.Item onClick={HandleProduct}>All</NavDropdown.Item>
+            <NavDropdown.Item>Archive</NavDropdown.Item>
+            <NavDropdown.Item>Sold Out</NavDropdown.Item>
+          </NavDropdown>
+        </div>
+      </div>
+
+      <hr className="container" />
+      <section className="container d-flex px-5 justify-content-between ">
+        <p className={`${styles["title"]} col-4 d-flex justify-content-start ms-4 `}>Product</p>
+        <p className={`${styles["title"]} col-4 d-flex justify-content-start ms-4 `}> Stock Status</p>
+        <p className={`${styles["title"]} col-4 d-flex justify-content-start ms-4 `}>Price</p>
+      </section>
+      <hr className="container" />
+      <div className="container">
+        {data.length > 0 ? (
+          data.map((data, index) => {
+            const images = data.image;
+            const listImage = images.replace(/{/g, "").replace(/}/g, "");
+            let imageLink = [];
+
+            listImage.split(",").map((link) => imageLink.push(link));
+
+            console.log(imageLink[0]);
+
+            return <Table key={index} stock={data.stock} description={data.description} image={imageLink[0]} price={`${"IDR"} ${costing(data.price)}`} />;
+          })
+        ) : (
+          <>
+            <Loader />
+          </>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default SellerProduct;
