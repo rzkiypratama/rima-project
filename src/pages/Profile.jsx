7c01344376c031d507a6248956564dd2bc35e984
imbasri.dev <imbasri.dev@gmail.com>
2022-11-21 17:18:03 +0700

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -37,12 +37,9 @@ function Profile() {
 
    const handleClose = () => setShow(false);
    const handleShow = () => setShow(true);
-
    const handleClosePassword = () => setShowPassword(false);
    const handleShowPassword = () => setShowPassword(true);
-
    const userInfo = JSON.parse(localStorage["userInfo"] || "{}");
-
    const toLogout = () => navigate("/login");
    const logoutHandler = (e) => {
       e.preventDefault();
